6e3e23e3d5613ccdb6d32207549a43e99339b320
nikunjagarwal321 <nikunjagarwal321@gmail.com>
2025-05-01 01:53:50 -0500

    Updated main exp

--- retrievers/hybrid_retriever.py
+++ retrievers/hybrid_retriever.py
@@ -1,19 +1,19 @@
 from .bm25_retriever import BM25Retriever
-from .dense_retriever import DPRRetriever
+from .colbert_retriever import ColBERTRetriever
 
 class HybridRetriever:
     def __init__(self, documents):
         # Initialize both BM25 and Dense retrievers
         self.bm25_retriever = BM25Retriever(documents)
-        self.dpr_retriever = DPRRetriever(documents)
+        self.colbert_retriever = ColBERTRetriever(documents)
 
     def retrieve(self, query, top_k=5):
         # Retrieve using BM25 and Dense Retriever
         bm25_results = self.bm25_retriever.retrieve(query, top_k)
-        dpr_results = self.dpr_retriever.retrieve(query, top_k)
+        colbert_results = self.colbert_retriever.retrieve(query, top_k)
 
         # Combine results, you can also add a re-ranking mechanism here
-        combined_results = list(set(bm25_results + dpr_results))
+        combined_results = list(set(bm25_results + colbert_results))
 
         # Return top-k results (after combining and re-ranking)
         return combined_results[:top_k]
